fcadbcfa3c261b18feabf2dfc7662ea50b7d3a3f
Julian Rodriguez <julian rodriguez19961@gmail.com>
2022-08-08 16:44:22 -0500

    Se configura metodo post para envio de formulario con Insert y Update

--- config/Commit.php
+++ config/Commit.php
@@ -3,10 +3,6 @@
 include('config.php');
 
 #Captura Variables
-$Dni = isset($_POST["username"])?$_POST["username"]:'';
-
-
-
 $Dni = isset($_POST["Dni"])?$_POST["Dni"]:'';
 $Name = isset($_POST["Name"])?$_POST["Name"]:'';
 $LastName = isset($_POST["LastName"])?$POST["LastName"]:'';
@@ -16,6 +12,7 @@ $CommentHour = isset($_POST["CommentHour"])?$_POST["CommentHour"]:'';
 $Diagnosis = isset($_POST["Diagnosis"])?$_POST["Diagnosis"]:'';
 $Accept = isset($_POST["Accept"])?$_POST["Accept"]:'';
 $Eps = isset($_POST["Eps"])?$_POST["Eps"]:'';
+$Range = isset($_POST["Range"])?$_POST["Range"]:'';
 $StatusEps = isset($_POST["StatusEps"])?$_POST["StatusEps"]:'';
 $SentBy = isset($_POST["SentBy"])?$_POST["SentBy"]:'';
 $Ips = isset($_POST["Ips"])?$_POST["Ips"]:'';
@@ -23,10 +20,15 @@ $AppointmentDate = isset($_POST["AppointmentDate"])?$_POST["AppointmentDate"]:''
 $AppointmentHour = isset($_POST["AppointmentHour"])?$_POST["AppointmentHour"]:'';
 $CallsNumber = isset($_POST["CallsNumber"])?$_POST["CallsNumber"]:'';
 $Comment = isset($_POST["Comment"])?$_POST["Comment"]:'';
+$TimeStamp = strtotime("now");
 
+#Insert
+$Sql = "INSERT INTO BitPriorities ('PK_UUID','FK_Patient','FK_EPS','FK_Range','FK_Ips','FK_Diagnosis','Dni','Name0','LastName0','SentBy','Comment0','CommentDate','AppointmentDate','Accept','StatusEps','AppointmentHour','CommentHour','CallsNumber','imageb64','z_xOne','created','updated','createdUser','updatedUser') VALUES ('1', $Dni, $Eps, $Range, $Ips, $Diagnosis, $Dni,$Name,$LastName, $SentBy,$Comment,$CommentDate,$AppointmentDate,$Accept,$StatusEps,$AppointmentHour,$CommentHour,$CallsNumber,'1',1,$TimeStamp ,'','Julian','')"; 
 
-$Sql = "INSERT INTO BitPriorities ('PK_UUID', 'FK_Patient', 'FK_EPS', 'FK_Range', 'FK_Ips', 'FK_Diagnosis', 'Dni', 'Name0', 'LastName0','SentBy', 'Comment0'  , 'CommentDate' ,'AppointmentDate','Accept', 'StatusEps','AppointmentHour','CommentHour','CallsNumber','imageb64','z_xOne','created','updated','createdUser','updatedUser')
-) VALUES ( UUID(), 'ABC',$Eps, , ) 
-"
+if(mysqli_query($conn, $Sql)){
+       echo "Records added successfully.";
+   } else{
+       echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+   }
+ ?>
 
-?>
