4228a3d7dec152d5b0a87a957bcd7815a5e63904
Julián Rodríguez <julianrodriguez19961@gmail.com>
2022-08-11 16:18:18 -0500

    Se configura listado para diagnostiscos luego de realizar un insert de 12440 registros

--- config/Commit.php
+++ config/Commit.php
@@ -29,7 +29,7 @@ $Time0 = date('G');
 
 #Insert
 $Sql = "INSERT INTO BitPriorities (PK_UUID,FK_Patient,FK_EPS,FK_Range,FK_Ips,FK_Diagnosis,Dni,Name0,LastName0,SentBy,Comment0,CommentDate,AppointmentDate,Accept,StatusEps,AppointmentHour,CommentHour,CallsNumber,imageb64,z_xOne,created,updated,createdUser,updatedUser) 
-VALUES ('2', '$Dni', '$Eps', '$Range', '$Ips', '$Diagnosis', '$Dni','$Name','$LastName0', '$SentBy','$Comment','$CommentDate','$AppointmentDate','$Accept','$StatusEps','$AppointmentHour','$CommentHour','$CallsNumber','1',1,'$Time0' ,'','Julian','')"; 
+VALUES (UUID(), '$Dni', '$Eps', '$Range', '$Ips', '$Diagnosis', '$Dni','$Name','$LastName0', '$SentBy','$Comment','$CommentDate','$AppointmentDate','$Accept','$StatusEps','$AppointmentHour','$CommentHour','$CallsNumber','1',1,'$Time0' ,'','Julian','')"; 
 // echo "$Sql <br>";
 
 
