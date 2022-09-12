72332930cdadb54cf81de28c2464700d4b3580f7
Julian Rodriguez <julian rodriguez19961@gmail.com>
2022-09-12 15:01:52 -0500

    Cambios 1.22 main;

--- config/setup.php
+++ config/setup.php
@@ -1,6 +1,6 @@
 <?php 
 $index ='http://192.168.1.22/samebit/';
 $url = '/pages/login.php';
-$urldashboard ='http://localhost/samebit/pages/dashboard.php';
+$urldashboard ='http:/192.168.1.22/samebit/pages/dashboard.php';
 $title = 'Same-Bit';
 ?>
