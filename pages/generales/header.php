5d1b28a1f1e30ba74b8b31a501604af09c9b4566
Julian Rodriguez <julian rodriguez19961@gmail.com>
2022-11-03 17:01:46 -0500

    031122.0.1.2:se agregan validaciones de creación y eliminación del registro para que si se tienen registros en el kardex no permita eliminar, pero sino los tiene si realice la eliminación

--- pages/generales/header.php
+++ pages/generales/header.php
@@ -1,47 +1,50 @@
- <?php 
-include '../config/setup.php';
-session_start();
-?>
+ <?php
+  include '../config/setup.php';
+  session_start();
+  ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-<title><?php echo $title;?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1" http-equiv="content-type" content="text/html; charset=UTF-8">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-   
-   <?php 
-    if ( empty($_SESSION['usuario']) ){
+ <!DOCTYPE html>
+ <html>
+
+ <head>
+   <title><?php echo $title; ?></title>
+   <meta name="viewport" content="width=device-width, initial-scale=1" http-equiv="content-type" content="text/html; charset=UTF-8">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+   <?php
+    if (empty($_SESSION['usuario'])) {
       // No hacer nada si viene vacio
-    }else{
-      $nombre = isset($_POST[$_SESSION['usuario']])?$_SESSION['usuario']:'1';
+    } else {
+      $nombre = isset($_POST[$_SESSION['usuario']]) ? $_SESSION['usuario'] : '1';
     }
     //  Identificar pagina
-     $link = $_SERVER['PHP_SELF'];
-     $link_array = explode('/',$link);
-     $page = end($link_array);
+    $link = $_SERVER['PHP_SELF'];
+    $link_array = explode('/', $link);
+    $page = end($link_array);
 
     // Cargar estilo dependiendo de página
-     if($page =='login.php'){ ?>
-        <link href="../css/login.css" rel="stylesheet" type="text/css">
-    <?php } else{?> 
-        <link href="../css/pacientes.css" rel="stylesheet" type="text/css">
-   <?php }?>
-      
+    if ($page == 'login.php') { ?>
+     <link href="../css/login.css" rel="stylesheet" type="text/css">
+   <?php } else { ?>
+     <link href="../css/pacientes.css" rel="stylesheet" type="text/css">
+   <?php } ?>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-  <script src="http://code.highcharts.com/highcharts.js"></script>
-  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"></script>
-</head>
-<body>
-    
 
-</body>
-</html>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+   <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+   <script src="http://code.highcharts.com/highcharts.js"></script>
+   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"></script>
+ </head>
+
+ <body>
+
+
+ </body>
+
+ </html>
--- pages/medicines_l.php
+++ pages/medicines_l.php
@@ -38,21 +38,21 @@ include './generales/header.php';
 
 <div class="row m-1">
 
-        <div class="d-inline-flex flex-row col-12 mt-2">
-            <div class="rows">
-                <div class="col-auto mx-1">
-                    <a href="./medicine_c.php" class="btn btn-md btn-primary" aria-current="page">Nuevo</a>
-                </div>
-                <!-- {{$rows->withQueryString()->links()}} -->
-            </div>
-
-            <div class="col-4 ms-auto">
-                <input type="text" class="form-control" name="searchbox" id="searchbox" placeholder="Busqueda por nombre y código" value="">
-            </div>
+    <div class="d-inline-flex flex-row col-12 mt-2">
+        <div class="rows">
             <div class="col-auto mx-1">
-                <button id="searchbtn" type="submit" class="btn btn-md btn-primary"> Buscar </button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-record" data-bs-whatever="@fat">Nuevo</button>
             </div>
+            <!-- {{$rows->withQueryString()->links()}} -->
         </div>
+
+        <div class="col-4 ms-auto">
+            <input type="text" class="form-control" name="searchbox" id="searchbox" placeholder="Busqueda por nombre y código" value="">
+        </div>
+        <div class="col-auto mx-1">
+            <button id="searchbtn" type="submit" class="btn btn-md btn-primary"> Buscar </button>
+        </div>
+    </div>
 
 </div>
 
@@ -70,11 +70,41 @@ include './generales/header.php';
                     <!-- <x-layouts.tables.columns :columns="$columns" /> -->
                 </tr>
             </thead>
-            <tbody id="dataMedicines" >
-                
+            <tbody id="dataMedicines">
             </tbody>
         </table>
     </div>
-</div>
-<script src="../Js/medicines.js"></script>
 
+    <div class="modal fade" id="modal-record" tabindex="-1" aria-labelledby="modal-record" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header text-white text-center bg-primary" id="modal-head">
+                    <h5 class="modal-title col">Nuevo Medicamento</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form id="medicineStored">
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="name" class="col-form-label">Nombre:</label>
+                            <input type="text" class="form-control" id="name" name="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="reference" class="col-form-label">Referencia:</label>
+                            <input type="text" class="form-control" id="reference" name="reference">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Observación:</label>
+                            <textarea class="form-control" name="observation" id="observation" rows="10"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary" id="stored">Guardar</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="../Js/medicines.js"></script>
