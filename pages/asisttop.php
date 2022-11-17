46ca0b6588e1f245621c725dbfc103995bd3e7cf
Julian Rodriguez <julian rodriguez19961@gmail.com>
2022-11-17 11:23:40 -0500

    cambios de navegación

--- pages/asisttop.php
+++ pages/asisttop.php
@@ -7,21 +7,10 @@ include './generales/header.php';
       <img src="../img/SameinLogo.png" height="40" class="logo">
     </a>
     <ul class="nav nav-pills">
-            <li class="nav-item"><a href="./dashboard.php" class="nav-link active" aria-current="page">Prioritaria</a></li>
+            <li class="nav-item"><a href="./dashboard.php" class="nav-link active" aria-current="page">Samebit</a></li>
             <li class="nav-item"><a href="./asisttop.php" class="nav-link" aria-current="page">Asist</a></li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" title="Farmacia">
-                    Farmacia
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="./medicines_l.php">Medicamentos</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="#">Reportes</a></li>
-                </ul>
-            </li>
-            <li class="nav-item"><a href="../config/logout.php" class="nav-link">Cerrar Sesión</a></li>
+            <li class="nav-item"><a href="./medicines_l.php" class="nav-link" aria-current="page">Samecomed</a></li>
+            <li class="nav-item"><a href="../config/logout.php" class="nav-link link-secondary">Cerrar Sesión</a></li>
         </ul>
 </header>
 
