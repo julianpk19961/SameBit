7ad4571b008d3945f394c0e03c8cd5e72153bcb3
Julian Rodriguez <julianrodriguez19961@gmail.com>
2023-03-01 17:04:50 -0500

    fix: :bug: Solcuón a problema con conversión de formato de fecha
    Se agrega función para captura el utc local y luego convertir la fecha recibida de tal forma que recalcule la fecha y establezca en base al pais

--- config/config.php
+++ config/config.php
@@ -1,21 +1,21 @@
 <?php
 // Datos sesion
-$host="localhost";
-$port=3306;
-$socket="";
-$user="usrconect";
-$password="toor";
-$dbname="samebit";
+$host = "localhost";
+$port = 3306;
+$socket = "";
+$user = "usrconect";
+$password = "toor";
+$dbname = "samebit";
 
 #Estabelercer conexión
 $conn = new mysqli($host, $user, $password, $dbname, $port, $socket);
 
-  if (!$conn) {
-    die("No hay conexión: ".mysqli_connect_error());
-  }
+if (!$conn) {
+  die("No hay conexión: " . mysqli_connect_error());
+}
 
-  //cambiar el conjunto de caracteres a utf8 
-  if(!$conn->set_charset("utf8")){
-    printf("Error cargando el conjunto de caracteres utf8: %\n", $conexion->error);
-    exit();
-  }
+//cambiar el conjunto de caracteres a utf8 
+if (!$conn->set_charset("utf8")) {
+  printf("Error cargando el conjunto de caracteres utf8: %\n", $conexion->error);
+  exit();
+}
--- config/getPriorities.php
+++ config/getPriorities.php
@@ -1,6 +1,41 @@
 <?php
 
 include 'config.php';
+date_default_timezone_set('America/Bogota');
+
+$dni = isset($_POST["dni"]) ? $_POST["dni"] : '';
+$user = isset($_POST["user"]) ? $_POST["user"] : '';
+$checkinStart = isset($_POST["checkinStart"]) ? $_POST["checkinStart"] : '';
+$checkinEnd = isset($_POST["checkinEnd"]) ? $_POST["checkinEnd"] : '';
+$checkOutStart = isset($_POST["checkOutStart"]) ? $_POST["checkOutStart"] : '';
+$checkOutEnd = isset($_POST["checkOutEnd"]) ? $_POST["checkOutEnd"] : '';
+$appointmentStart = isset($_POST["appointmentStart"]) ? $_POST["appointmentStart"] : '';
+$appointmentEnd = isset($_POST["appointmentEnd"]) ? $_POST["appointmentEnd"] : '';
+
+
+if ($checkinStart) {
+
+    $checkinStart = date("Y-m-d H:i:s", strtotime($checkinStart));
+
+    if ($checkinEnd) {
+        $checkinEnd = date("Y-m-d H:i:s", strtotime($checkinEnd));
+    }
+
+    $checkInDateWhere = !$checkinEnd ?
+        "checkIn_date = $checkinStart" :
+        "CONCAT(checkIn_date,' ',checkIn_time) BETWEEN '$checkinStart' AND '$checkinEnd'";
+}
+
+if ($checkOutStart and !$checkinEnd) {
+    $checkOutStart = date("Y-m-d H:i:s", strtotime($checkOutStart));
+    // $checkinEnd = date_time_set($checkOutStart, 23, 45, 49);
+}
+
+if ($appointmentStart) {
+    $appointmentStart = date("Y-m-d H:i:s", strtotime($appointmentStart));
+    // $chappointmentEndckinEnd = date_time_set($appointmentStart, 23, 45, 49);
+
+}
 
 $sql = "SELECT CONCAT(p.Name0 ,' ',p.lastname0) as PACIENTE, e.name0 as EPS,
 b.statuseps ESTADO_EPS, b.FK_Range as RANGO, i.name0 AS IPS, d.codigo  AS DIAGNOSTICO,
@@ -19,8 +54,25 @@ ON b.FK_EPS = e.PK_UUID
 INNER JOIN entities i
 ON b.FK_Ips = i.PK_UUID
 INNER JOIN diagnosis d
-ON b.FK_Diagnosis = d.KP_UUID";
+ON b.FK_Diagnosis = d.KP_UUID
 
+WHERE $checkInDateWhere
+";
+
+$data['data'] = [
+    'dni' => $dni,
+    'user' => $user,
+    'checkinStart' => $checkinStart,
+    'checkinEnd' => $checkinEnd,
+    'checkInDateWhere' => $checkInDateWhere,
+    'checkOutStart' => $checkOutStart,
+    'checkOutEnd' => $checkOutEnd,
+    'appointmentStart' => $appointmentStart,
+    'appointmentEnd' => $appointmentEnd,
+    'sql' => $sql,
+];
+echo print_r($data);
+return false;
 
 $result = mysqli_query($conn, $sql);
 
